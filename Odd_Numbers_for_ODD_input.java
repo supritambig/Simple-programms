6a03993b43fae0fba4c2b68a091d7478e9c7abc1
Suprit Ambig <127177031+supritambig@users.noreply.github.com>
2025-11-27 13:43:34 +0530

    Implement program to print odd numbers for odd input
    This program prompts the user for an integer and prints the first N odd numbers, where N is the largest odd number less than or equal to the input.

---
+++ Odd_Numbers_for_ODD_input.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+
+        if (a % 2 == 0)
+            a = a - 1;
+
+        int count = (a + 1) / 2;
+        int num = 1;
+
+        for (int i = 1; i <= count; i++) {
+            System.out.print(num + " ");
+            num += 2;
+        }
+    }
+}
