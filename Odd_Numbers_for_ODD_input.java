939a42a3020aafb0e0e4263bcd74afe0b16b449f
Suprit Ambig <127177031+supritambig@users.noreply.github.com>
2025-11-27 13:46:56 +0530

    Rename class to Odd_Numbers_for_ODD_input

--- Odd_Numbers_for_ODD_input.java
+++ Odd_Numbers_for_ODD_input.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Problem3 {
+public class Odd_Numbers_for_ODD_input {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a: ");
