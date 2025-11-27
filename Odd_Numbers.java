82a517c91c923ccd2a2be4d19a0374be196cb29c
Suprit Ambig <127177031+supritambig@users.noreply.github.com>
2025-11-27 13:39:40 +0530

    Implement Odd_Numbers program to print odd numbers

---
+++ Odd_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+
+        int num = 1;
+        for (int i = 1; i <= a; i++) {
+            System.out.print(num + " ");
+            num += 2;
+        }
+    }
+}
